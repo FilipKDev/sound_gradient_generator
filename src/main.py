fdd1037c90a16e3e1eda7e696a24a54942fee448
FilipKDev <filip.kwasniewski@gmail.com>
2025-06-23 22:08:56 +0100

    1. created slider and cursor classes to be used for UI elements in the application 2. Minor code refactoring and fixes

--- src/main.py
+++ src/main.py
@@ -15,21 +15,21 @@ class Window:
         self.__root.geometry(f"{self._width}x{self._height+50}")
         self._im = GradientImage(self._width, self._height, self._start, self._stop, self.__root)
         self._im.generate_image()
+
         self._v1 = DoubleVar()
-        #self.canvas = Canvas(self.__root, width = width, height = 45)
-        #self.canvas.place(x=0, y=self._height+5)
-        self._create_buttons_testing()
+        
+        self.canvas = Canvas(self.__root, width = self._width, height = 50)
+        self.canvas.place(x=0, y=self._height)
+        self._slider = Slider(x=5, y=5, thickness=40, length=self._width-10, orientation=HORIZONTAL, canvas=self.canvas)
+        #self.canvas.bind('<B1-Motion>', self._motion_test)
+        #self._create_buttons_testing()
         #self._create_slider_testing()
+
+    def _motion_test(self, event):
+        print(f"{event.x} {event.y}")
 
     def run(self):
         self.__root.mainloop()
-
-    def test(self):
-        print("Test button pressed")
-        print(self._width)
-        print(self._height)
-        print(self._start)
-        print(self._stop)
 
     def _display_data(self):
         print(self._im.line)
@@ -108,6 +108,88 @@ class GradientImage:
         self.line = self.line[:-self._increment]
         self.generate_image()
 
+class Slider:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+        self._x = self.__dict__.get('x', 0)
+        self._y = self.__dict__.get('y', 0)
+        self._length = self.__dict__.get('length', 10)
+        self._thickness = self.__dict__.get('thickness', 10)
+        self._orientation = self.__dict__.get('orientation', VERTICAL)
+        self._canvas = self.__dict__.get('canvas', None)
+        self._create_frame()
+
+        self._cursor_width = self.__dict__.get('cursor_width', 10)
+        self._cursor = None
+        self._create_cursor()
+
+    def _create_frame(self):
+        if not self._canvas:
+            return
+        self._line_width = 2
+        if self._orientation == VERTICAL:
+            self._canvas.create_rectangle(self._x, self._y, self._x+self._thickness, self._y+self._length, outline="black", width=self._line_width)
+        elif self._orientation == HORIZONTAL:
+            self._canvas.create_rectangle(self._x, self._y, self._x+self._length, self._y+self._thickness, outline="black", width=self._line_width)
+
+    def _create_cursor(self):
+        if not self._canvas:
+            return   
+        if self._orientation == VERTICAL:
+            self._cursor = Cursor(self._x, 
+                                  self._y, 
+                                  self._x+self._thickness, 
+                                  self._y+self._cursor_width, 
+                                  self._canvas, 
+                                  self._cursor_width,
+                                  self._y,
+                                  self._y+self._length)
+        elif self._orientation == HORIZONTAL:
+            self._cursor = Cursor(self._x, 
+                                  self._y, 
+                                  self._x+self._cursor_width, 
+                                  self._y+self._thickness, 
+                                  self._canvas,
+                                  self._cursor_width,
+                                  self._x,
+                                  self._x+self._length)
+            self._canvas.tag_bind(self._cursor.get_object(), '<Button-1>', self._cursor.calculate_offset)
+            self._canvas.tag_bind(self._cursor.get_object(), '<B1-Motion>', self._cursor.move_x)
+            self._canvas.tag_bind(self._cursor.get_object(), '<B1-ButtonRelease>', self._cursor.unclick)
+        self._canvas.tag_bind(self._cursor.get_object(), '<Enter>', self._cursor.highlight_on)
+        self._canvas.tag_bind(self._cursor.get_object(), '<Leave>', self._cursor.highlight_off)
+
+class Cursor:
+    def __init__(self, x1, y1, x2, y2, canvas, cursor_width, boundary_min, boundary_max):
+        self.__root = canvas.create_rectangle(x1, y1, x2, y2, fill="grey")
+        self._canvas = canvas
+        self._cursor_width = cursor_width
+        self._boundary_min = boundary_min
+        self._boundary_max = boundary_max
+        self._offset_x = 0
+        self._clicked = False
+
+    def unclick(self, event):
+        self._clicked = False
+
+    def highlight_on(self, event):
+        self._canvas.itemconfig(self.__root, fill="yellow")
+
+    def highlight_off(self, event):
+        if not self._clicked:
+            self._canvas.itemconfig(self.__root, fill="grey")
+
+    def get_object(self):
+        return self.__root
+
+    def move_x(self, event):
+        self._canvas.moveto(self.__root, x=min(self._boundary_max-1 - self._cursor_width, max(self._boundary_min-1, event.x-self._offset_x)))
+
+    def calculate_offset(self, event):
+        self._offset_x = event.x - self._canvas.coords(self.__root)[0]
+        self._clicked = True
+
 def test_image():
     line = np.linspace(0, 255, 100, True, False, np.uint8)
     array = np.tile(line, (100, 1))
@@ -131,7 +213,8 @@ def three_point_gradient(start, stop, width):
     return np.concatenate((line1, line2))
 
 def main():
-    params = input("Enter parameters: ")
+    #params = input("Enter parameters: ")
+    params = "1000 1000 0 100"
     generate_test_image(*params.split())
     print("Complete!")
 
